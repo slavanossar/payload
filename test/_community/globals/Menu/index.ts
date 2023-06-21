1d5a353a672ba3e9ce556fe8b27b1906dc77cf99
Slava Nossar <slava.nossar@gmail.com>
2023-06-21 16:11:37 +1000

    add fields

--- test/_community/globals/Menu/index.ts
+++ test/_community/globals/Menu/index.ts
@@ -1,13 +1,44 @@
-import { GlobalConfig } from '../../../../src/globals/config/types';
+import { GlobalConfig } from "../../../../src/globals/config/types";
 
-export const menuSlug = 'menu';
+export const menuSlug = "menu";
 
 export const MenuGlobal: GlobalConfig = {
   slug: menuSlug,
   fields: [
     {
-      name: 'globalText',
-      type: 'text',
+      name: "globalText",
+      type: "text",
+    },
+    {
+      type: "tabs",
+      tabs: [
+        {
+          label: "Page",
+          fields: [
+            {
+              type: "tabs",
+              admin: {
+                condition: (data) => {
+                  console.log("loop");
+                  return data.globalText === "abc";
+                },
+              },
+              tabs: [
+                {
+                  label: "Tab",
+                  fields: [
+                    {
+                      name: "conditionalField",
+                      label: "Conditional Field",
+                      type: "text",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
     },
   ],
 };
